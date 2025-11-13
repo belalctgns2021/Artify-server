be087b9ef862216ad7e50e2bba1030323ce4f30d
Belal Hossain <belalctgns2021@gmail.com>
2025-11-13 22:55:41 +0600

    Server finished

--- .history/index_20251113225219.js
+++ .history/index_20251113225219.js
@@ -1,6 +1,7 @@
+
+
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
@@ -11,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vyznij5.mongodb.net/?appName=Cluster0";
+const uri = "mongodb+srv://${process.env.DB_USE}:${process.env.DB_PASS}@cluster0.vyznij5.mongodb.net/?appName=Cluster0";
 
 
 const client = new MongoClient(uri, {
